fa13edb8802ccdf2e3bc53183b35d43976e4cd25
EEBroadband <107469677+EEBroadband@users.noreply.github.com>
2024-05-03 22:11:14 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -44,7 +44,7 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Update v3.6`, 
+  name: `me`, 
   type: ActivityType.Watching }), 10000);
 client.errorLog = config.errorLog
 } else {
